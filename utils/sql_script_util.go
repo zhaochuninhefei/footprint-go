cf3628d759296a9206c443cb3e45569641017902
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-02-03 16:37:53 +0800

    调试sql脚本的执行

--- utils/sql_script_util.go
+++ utils/sql_script_util.go
@@ -1,11 +1,31 @@
 package utils
 
-import "strings"
+import (
+	"gitee.com/zhaochuninhefei/zcgolog/zclog"
+	"gorm.io/gorm"
+	"strings"
+)
+
+func RunSqlScript(dbClient *gorm.DB, sqlScript string) error {
+	sqls, err := ReadSqls(sqlScript)
+	if err != nil {
+		return err
+	}
+	for _, sql := range sqls {
+		zclog.Infof("执行sql: %s", sql)
+		db := dbClient.Exec(sql)
+		if err = db.Error; err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
 
 // ReadSqls 读取SQL脚本文件
 //  @param sqlScript SQL脚本文件内容
 //  @return []string SQL语句集合
-func ReadSqls(sqlScript string) []string {
+func ReadSqls(sqlScript string) ([]string, error) {
 	sqls := make([]string, 0)
 	// 按行读取SQL内容
 	lines := splitLines(sqlScript)
@@ -21,15 +41,24 @@ func ReadSqls(sqlScript string) []string {
 		if strings.HasSuffix(lineTrim, ";") {
 			// 如果该行以";"结尾，则认为该条sql语句结束
 			// 先去除末尾分号，将该行加入sql构造器
-			sqlBuilder.WriteString(lineTrim[:len(lineTrim)-1])
+			_, err := sqlBuilder.WriteString(lineTrim[:len(lineTrim)-1])
+			if err != nil {
+				return nil, err
+			}
 			// 将sql构造器转为sql语句，加入sql语句集合
 			sqls = append(sqls, sqlBuilder.String())
 			// 重置sql构造器
 			sqlBuilder.Reset()
 		} else {
 			// 如果该行没有以";"结尾，则认为该条sql语句尚未结束
-			sqlBuilder.WriteString(lineTrim)
-			sqlBuilder.WriteString(" \n")
+			_, err := sqlBuilder.WriteString(lineTrim)
+			if err == nil {
+				_, err = sqlBuilder.WriteString(" \n")
+			}
+			if err != nil {
+				return nil, err
+			}
+
 		}
 	}
 	// 特殊场景处理：如果sql脚本最后一条sql语句没有写";"结尾，则需要将非空的sql构造器转为sql语句并加入sql语句集合。
@@ -37,7 +66,7 @@ func ReadSqls(sqlScript string) []string {
 		sqls = append(sqls, strings.TrimSpace(sqlBuilder.String()))
 		sqlBuilder.Reset()
 	}
-	return sqls
+	return sqls, nil
 }
 
 func splitLines(sqlScript string) []string {
--- utils/sql_script_util_test.go
+++ utils/sql_script_util_test.go
@@ -13,12 +13,15 @@ func TestReadSqls(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	scriptSQL, err := resources.DBFilesTest.ReadFile("db/beforeclass/clear_footprinttest.sql")
+	scriptSQL, err := resources.DBFilesTest.ReadFile("db/test01/smtp_V1.0.0_init.sql")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	sqls := ReadSqls(string(scriptSQL))
+	sqls, err := ReadSqls(string(scriptSQL))
+	if err != nil {
+		t.Fatal(err)
+	}
 	for _, sql := range sqls {
 		fmt.Printf("读取到sql: %s\nend\n", sql)
 	}
@@ -29,4 +32,39 @@ func TestReadSqls(t *testing.T) {
 			t.Fatal(err)
 		}
 	}
+}
+
+//goland:noinspection SqlResolve
+func TestRunSqlScript(t *testing.T) {
+	mysqlClient, err := mysql.ConnectMysqlByDefault(nil, "localhost", "3307", "zhaochun1", "zhaochun@GITHUB", "db_footprint_test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	scriptSQL, err := resources.DBFilesTest.ReadFile("db/test01/smtp_V1.0.0_init.sql")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = RunSqlScript(mysqlClient, string(scriptSQL))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	data := make([]string, 0)
+	mysqlClient.Raw("SELECT smtp_host from rv_smtps").Scan(&data)
+	fmt.Printf("查询到数据: %s\n", data)
+
+	scriptSQL2, err := resources.DBFilesTest.ReadFile("db/test02/smtp_V1.0.999_add_smtp01.sql")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = RunSqlScript(mysqlClient, string(scriptSQL2))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	mysqlClient.Raw("SELECT smtp_host from rv_smtps").Scan(&data)
+	fmt.Printf("查询到数据: %s\n", data)
 }

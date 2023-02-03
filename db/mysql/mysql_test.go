a052205fb1c68f800ffb4c9ee04a906b84e2946e
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-02-03 15:49:02 +0800

    调试sql脚本的执行

--- db/mysql/mysql_test.go
+++ db/mysql/mysql_test.go
@@ -1,6 +1,7 @@
 package mysql
 
 import (
+	"gitee.com/zhaochuninhefei/footprint-go/test/resources"
 	"gitee.com/zhaochuninhefei/zcgolog/zclog"
 	"testing"
 )
@@ -9,4 +10,21 @@ func TestConnectMysqlByDefault(t *testing.T) {
 	zclog.Info("===== TestConnectMysqlByDefault 开始 =====")
 	RunDBTest()
 	zclog.Info("===== TestConnectMysqlByDefault 结束 =====")
+}
+
+func TestExecScriptFile(t *testing.T) {
+	scriptSQL, err := resources.DBFilesTest.ReadFile("db/beforeclass/clear_footprinttest.sql")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	mysqlClient, err := ConnectMysqlByDefault(nil, "localhost", "3307", "zhaochun1", "zhaochun@GITHUB", "db_footprint_test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	db := mysqlClient.Exec(string(scriptSQL))
+	if err = db.Error; err != nil {
+		t.Fatal(err)
+	}
 }

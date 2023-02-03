8bd3e5d323ee56b1df8bae8bcab383103102d82f
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-02-03 11:41:27 +0800

    重构versionctl

--- versionctl/versionctl.go
+++ versionctl/versionctl.go
@@ -1,5 +1,13 @@
 package versionctl
 
+import (
+	"embed"
+	"fmt"
+	"path/filepath"
+	"regexp"
+	"strconv"
+)
+
 type EmbedSqlFileInfo struct {
 	// sql脚本文件名
 	//  格式为"[业务空间]_V[major].[minor].[patch].[extend]_[自定义名称].sql"
@@ -26,4 +34,119 @@ type EmbedSqlFileInfo struct {
 	Version string `json:"version"`
 	// 该sql脚本的自定义名称，支持大小写字母，数字与下划线
 	CustomName string `json:"custom_name"`
+}
+
+// PTN_SCRIPT_NAME_DEFAULT sql脚本文件名正则表达式(默认)
+//goland:noinspection GoSnakeCaseUsage
+var PTN_SCRIPT_NAME_DEFAULT *regexp.Regexp
+
+// PTN_SCRIPT_NAME_EXTEND sql脚本文件名正则表达式(带扩展版本号)
+//goland:noinspection GoSnakeCaseUsage
+var PTN_SCRIPT_NAME_EXTEND *regexp.Regexp
+
+func init() {
+	PTN_SCRIPT_NAME_DEFAULT = regexp.MustCompile("^([A-Za-z0-9]+)_V(\\d+)\\.(\\d+)\\.(\\d+)_(\\w+)\\.sql$")
+	if PTN_SCRIPT_NAME_DEFAULT == nil {
+		panic("正则表达式不正确: ^([A-Za-z0-9]+)_V(\\d+)\\.(\\d+)\\.(\\d+)_(\\w+)\\.sql$")
+	}
+	PTN_SCRIPT_NAME_EXTEND = regexp.MustCompile("^([A-Za-z0-9]+)_V(\\d+)\\.(\\d+)\\.(\\d+)\\.(\\d+)_(\\w+)\\.sql$")
+	if PTN_SCRIPT_NAME_EXTEND == nil {
+		panic("正则表达式不正确: ^([A-Za-z0-9]+)_V(\\d+)\\.(\\d+)\\.(\\d+)\\.(\\d+)_(\\w+)\\.sql$")
+	}
+}
+
+func ReadEmbedFsByDirName(embedFs *embed.FS, dirPath string) ([]*EmbedSqlFileInfo, error) {
+	files := make([]*EmbedSqlFileInfo, 0)
+
+	entries, err := embedFs.ReadDir(dirPath)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entry := range entries {
+		name := entry.Name()
+		path := filepath.Join(dirPath, name)
+		isDir := entry.IsDir()
+		if isDir {
+			subFiles, err := ReadEmbedFsByDirName(embedFs, path)
+			if err != nil {
+				return nil, err
+			}
+			files = append(files, subFiles...)
+		} else {
+			fileBytes, err := embedFs.ReadFile(path)
+			if err != nil {
+				return nil, err
+			}
+			fileInfo := EmbedSqlFileInfo{
+				Name:    name,
+				Path:    path,
+				Content: string(fileBytes),
+			}
+			err = FilledDetailsFromSqlFileName(&fileInfo)
+			files = append(files, &fileInfo)
+		}
+	}
+	return files, nil
+}
+
+func FilledDetailsFromSqlFileName(fileInfo *EmbedSqlFileInfo) error {
+	matcherDefault := PTN_SCRIPT_NAME_DEFAULT.FindAllStringSubmatch(fileInfo.Name, -1)
+	if len(matcherDefault) > 0 {
+		for _, strMatched := range matcherDefault {
+			if len(strMatched) == 6 {
+				fileInfo.BusinessSpace = strMatched[1]
+				major, err := strconv.ParseInt(strMatched[2], 10, 64)
+				if err != nil {
+					return err
+				}
+				fileInfo.MajorVersion = major
+				minor, err := strconv.ParseInt(strMatched[3], 10, 64)
+				if err != nil {
+					return err
+				}
+				fileInfo.MinorVersion = minor
+				patch, err := strconv.ParseInt(strMatched[4], 10, 64)
+				if err != nil {
+					return err
+				}
+				fileInfo.PatchVersion = patch
+				fileInfo.CustomName = strMatched[5]
+				fileInfo.Version = fmt.Sprintf("%s_V%d.%d.%d", fileInfo.BusinessSpace, major, minor, patch)
+				return nil
+			}
+		}
+	} else {
+		matcherExtend := PTN_SCRIPT_NAME_EXTEND.FindAllStringSubmatch(fileInfo.Name, -1)
+		for _, strMatched := range matcherExtend {
+			if len(strMatched) == 7 {
+				fileInfo.BusinessSpace = strMatched[1]
+				major, err := strconv.ParseInt(strMatched[2], 10, 64)
+				if err != nil {
+					return err
+				}
+				fileInfo.MajorVersion = major
+				minor, err := strconv.ParseInt(strMatched[3], 10, 64)
+				if err != nil {
+					return err
+				}
+				fileInfo.MinorVersion = minor
+				patch, err := strconv.ParseInt(strMatched[4], 10, 64)
+				if err != nil {
+					return err
+				}
+				fileInfo.PatchVersion = patch
+				extend, err := strconv.ParseInt(strMatched[5], 10, 64)
+				if err != nil {
+					return err
+				}
+				fileInfo.ExtendVersion = extend
+				fileInfo.CustomName = strMatched[6]
+				fileInfo.Version = fmt.Sprintf("%s_V%d.%d.%d.%d", fileInfo.BusinessSpace, major, minor, patch, extend)
+				return nil
+			}
+		}
+	}
+
+	return fmt.Errorf("sqlFileName未能正确匹配正则表达式: %s", fileInfo.Name)
 }

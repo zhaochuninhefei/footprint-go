01a961e4899fb873dd3094cd24298154d7aba2a6
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-02-03 09:01:23 +0800

    添加embed文件测试用例

--- utils/embed_fs_util_test.go
+++ utils/embed_fs_util_test.go
@@ -82,4 +82,15 @@ func TestPrintEmbedFs(t *testing.T) {
 		fmt.Printf("文件名: %s, 访问路径: %s\n", fileInfo.name, fileInfo.path)
 		//fmt.Printf("文件名: %s, 访问路径: %s, \n文件内容:\n----------\n%s\n----------\n", fileInfo.name, fileInfo.path, fileInfo.content)
 	}
+
+	fmt.Println()
+	fmt.Println("db 下的文件")
+	files, err = ReadEmbedFsByDirName(&resources.DBFilesTest, "db")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, fileInfo := range files {
+		fmt.Printf("文件名: %s, 访问路径: %s\n", fileInfo.name, fileInfo.path)
+		//fmt.Printf("文件名: %s, 访问路径: %s, \n文件内容:\n----------\n%s\n----------\n", fileInfo.name, fileInfo.path, fileInfo.content)
+	}
 }

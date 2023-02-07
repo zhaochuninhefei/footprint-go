f991c8864bb23a8a5cd24a4a971c18fd8829e401
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-02-07 16:10:06 +0800

    删除多余main

--- db/mysql/mysql.go
+++ db/mysql/mysql.go
@@ -2,8 +2,6 @@ package mysql
 
 import (
 	"fmt"
-	"gitee.com/zhaochuninhefei/footprint-go/resources"
-	"gitee.com/zhaochuninhefei/zcgolog/zclog"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -17,38 +15,4 @@ func ConnectMysqlByDefault(dbExist *gorm.DB, host, port, user, pass, dbname stri
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		user, pass, host, port, dbname)
 	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
-}
-
-func RunDBTest() {
-	mysqlClient, err := ConnectMysqlByDefault(nil, "localhost", "3307", "zhaochun1", "zhaochun@GITHUB", "db_footprint_test")
-	if err != nil {
-		zclog.Errorln(err)
-		return
-	}
-
-	db := mysqlClient.Exec("drop table if exists `brood_db_version_ctl`")
-	if err := db.Error; err != nil {
-		zclog.Errorln(err)
-		return
-	}
-	tables := make([]string, 0)
-	mysqlClient.Raw("show tables").Scan(&tables)
-	zclog.Info(tables)
-
-	createSql, err := resources.DBFiles.ReadFile("db/versionctl/create_brood_db_version_ctl.sql")
-	if err != nil {
-		zclog.Errorln(err)
-		return
-	}
-
-	zclog.Info(string(createSql))
-
-	mysqlClient.Exec(string(createSql))
-	if err := db.Error; err != nil {
-		zclog.Errorln(err)
-		return
-	}
-	tables = make([]string, 0)
-	mysqlClient.Raw("show tables").Scan(&tables)
-	zclog.Info(tables)
 }
--- db/mysql/mysql_test.go
+++ db/mysql/mysql_test.go
@@ -1,6 +1,7 @@
 package mysql
 
 import (
+	"gitee.com/zhaochuninhefei/footprint-go/resources"
 	"gitee.com/zhaochuninhefei/zcgolog/zclog"
 	"testing"
 )
@@ -9,4 +10,39 @@ func TestConnectMysqlByDefault(t *testing.T) {
 	zclog.Info("===== TestConnectMysqlByDefault 开始 =====")
 	RunDBTest()
 	zclog.Info("===== TestConnectMysqlByDefault 结束 =====")
+}
+
+//goland:noinspection SqlNoDataSourceInspection
+func RunDBTest() {
+	mysqlClient, err := ConnectMysqlByDefault(nil, "localhost", "3307", "zhaochun1", "zhaochun@GITHUB", "db_footprint_test")
+	if err != nil {
+		zclog.Errorln(err)
+		return
+	}
+
+	db := mysqlClient.Exec("drop table if exists `brood_db_version_ctl`")
+	if err := db.Error; err != nil {
+		zclog.Errorln(err)
+		return
+	}
+	tables := make([]string, 0)
+	mysqlClient.Raw("show tables").Scan(&tables)
+	zclog.Info(tables)
+
+	createSql, err := resources.DBFiles.ReadFile("db/versionctl/create_brood_db_version_ctl.sql")
+	if err != nil {
+		zclog.Errorln(err)
+		return
+	}
+
+	zclog.Info(string(createSql))
+
+	mysqlClient.Exec(string(createSql))
+	if err := db.Error; err != nil {
+		zclog.Errorln(err)
+		return
+	}
+	tables = make([]string, 0)
+	mysqlClient.Raw("show tables").Scan(&tables)
+	zclog.Info(tables)
 }
